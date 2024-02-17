83c0f45d4af34517816eaa230930af798f23f01a
Christian Arévalo Jesús <carevalojesus@gmail.com>
2024-02-17 14:38:26 -0500

    actualizado cajero automatico

--- src/CajeroAutomatico.java
+++ src/CajeroAutomatico.java
@@ -15,11 +15,34 @@ public class CajeroAutomatico {
             System.out.println("2. Depositar dinero");
             System.out.println("3. Retirar dinero");
             System.out.println("4. Salir");
-            System.out.println("Seleccione una opción");
+            System.out.print("Seleccione una opción :");
 
             opcion = scanner.nextInt();
 
             System.out.println(opcion);
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Su saldo es: S/ " + saldo);
+                    break;
+                case 2:
+                    System.out.print("Ingrese la cantidad a depositar: ");
+                    int cantidad = scanner.nextInt();
+                    saldo += cantidad;
+                case 3:
+                    System.out.println("Ingrese la cantidad a retirar: ");
+                    cantidad = scanner.nextInt();
+
+                    if (cantidad > saldo) {
+                        System.out.println("Saldo insuficiente");
+                    } else {
+                        saldo -= cantidad;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar el cajero automático");
+                    break;
+            }
         }
 
     }
